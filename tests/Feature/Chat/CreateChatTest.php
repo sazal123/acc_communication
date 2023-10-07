f0b8e92e469692d9bf2a0b401d321f595f9425f1
giyas <gias@m.insta.kim>
2023-10-07 21:43:13 +0600

    feature test for create-chat

--- tests/Feature/Chat/CreateChatTest.php
+++ tests/Feature/Chat/CreateChatTest.php
@@ -4,38 +4,25 @@ namespace Tests\Feature\Chat;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
+use Exception;
 
 class CreateChatTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
-    public function user_can_create_a_chat_with_participants()
+    public function user_can_create_a_chat_with_participant()
     {
-        $otherUsers = User::factory()->count(2)->create();
 
+        $otherUsers = User::factory()->count(1)->create();
         $userIds = $otherUsers->pluck('id')->toArray();
-
-        // Data for the chat to be created
         $chatData = [
-            'users' => $userIds  // Passing user IDs to the API endpoint
+            'participantId' => $userIds[0]
         ];
+        $response = $this->post("api/create-chat", $chatData);
+        $response->assertStatus(200);
 
-        $response = $this->postJson("/api/create-chat", $chatData);
-
-        // Then: The API should respond with the chat details and status 201
-        $response->assertStatus(201)
-            ->assertJson([
-                'id' => 'chat id'
-            ]);
-
-        // Also, verify that the chat exists in the database with the given users
-        $createdChatId = $response->json('id');
-        $this->assertDatabaseHas('acc_com_chats', ['id' => $createdChatId]);
-        foreach ($userIds as $userId) {
-            $this->assertDatabaseHas('acc_com_participants', ['chat_id' => $createdChatId, 'user_id' => $userId]);
-        }
     }
 }
